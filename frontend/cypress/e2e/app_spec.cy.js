9966e89db73ef207c5a8f49adea17e56b04260ee
Megan Folsom <mfolsom@gmail.com>
2023-11-15 09:55:40 +0000

    fixed mistakes in my requires

--- frontend/cypress/e2e/app_spec.cy.js
+++ frontend/cypress/e2e/app_spec.cy.js
@@ -4,10 +4,4 @@ describe('App', () => {
         cy.url().should('include', '/login');
     });
 
-    it('redirects to /posts when user is logged in', () => {
-        cy.visit('/');
-        cy.login('someone@example.com', 'password');
-        cy.wait(7000); // wait for 7 seconds
-        cy.url().should('include', '/posts');
-    });
 });
